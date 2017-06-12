6fe052495e65fe3e963ac887011778abbf7936c4
stas <kelen.omnel1@gmail.com>
2017-06-13 02:06:34 +0300

    commit

---
+++ src/components/tag-item/tag-item.vue
@@ -0,0 +1,13 @@
+<style src="./tag-item.css" />
+
+<template>
+    <nav class="mainmenu">
+		    <div class="tagitem">
+            <div class="itemmenu"><a href="#">ALL</a></div>
+            <div class="itemmenu"><a href="#">WEB DESIGN</a></div>
+            <div class="itemmenu"><a href="#">LOGO DESIGN</a></div>
+            <div class="itemmenu"><a href="#">PHOTOGRAPHY</a></div>
+            <div class="itemmenu"><a href="#">WORDPRESS</a></div>
+		    </div> 
+    </nav>
+</template>
