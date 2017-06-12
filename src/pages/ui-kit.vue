e50009a59633ced404430329367fffbe07a07d24
stas <kelen.omnel1@gmail.com>
2017-06-13 02:00:16 +0300

    tag-item complate

--- src/pages/ui-kit.vue
+++ src/pages/ui-kit.vue
@@ -1,9 +1,10 @@
 <script>
   import PageHeader from '../components/page-header/page-header.vue';
   import ImageContent from '../components/image-content/image-content.vue';
+  import TagItem from '../components/tag-item/tag-item.vue';
 
   export default {
-    components: { PageHeader, ImageContent },
+    components: { PageHeader, ImageContent, TagItem },
   };
 </script>
 
@@ -14,6 +15,8 @@
     UI Kit
 
       <image-content></image-content>
+
+      <tag-item></tag-item>
 
     <a href="">Open Sans</a>
 
