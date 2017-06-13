a46bc0beac85b3e46ca2ab78580fd7638881d00f
Romserg <serg.romankov@gmail.com>
2017-06-13 23:41:47 +0300

    search-box-input in progress

--- src/components/search-box/search-box.vue
+++ src/components/search-box/search-box.vue
@@ -3,6 +3,6 @@
 <template>
   <div class="search-box">
 	 <div class="search-box-button"><slot></slot></div> 
-	 <div class="search-box-input"><slot>Default text</slot></div>
+	 <div class="search-box-input"><slot><input type="text"></slot></div>
   </div>  
 </template>
