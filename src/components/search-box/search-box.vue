56a15cea76b1efead50acf2b441d4270439ea5f5
Romserg <serg.romankov@gmail.com>
2017-06-13 22:41:42 +0300

    again )

--- src/components/search-box/search-box.vue
+++ src/components/search-box/search-box.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div class="search-box">
-	 <div class="search-box-button"><slot>Default button</slot></div> 
+	 <div class="search-box-button"><slot></slot></div> 
 	 <div class="search-box-input"><slot>Default text</slot></div>
   </div>  
 </template>
