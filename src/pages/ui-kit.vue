f697c5d48cbf31a8b13962c22959e135a2453064
Diana Ostrenko <ostrenkodiana1@gmail.com>
2017-06-13 13:34:19 +0300

    add main-logo

--- src/pages/ui-kit.vue
+++ src/pages/ui-kit.vue
@@ -2,21 +2,23 @@
 	import PageHeader from '../components/page-header/page-header.vue';
 	import ImageContent from '../components/image-content/image-content.vue';
 	import TagItem from '../components/tag-item/tag-item.vue';
+	import MainLogo from '../components/main-logo/main-logo.vue';
 
 	export default {
-		components: { PageHeader, ImageContent, TagItem }
+		components: { PageHeader, ImageContent, TagItem, MainLogo }
 	};
 </script>
 
 <template>
 	<body>
 		<page-header />
-
+		
 		UI Kit
 
 		<image-content></image-content>
 
 		<tag-item></tag-item>
+		
 
 		<a href="">Open Sans</a>
 
