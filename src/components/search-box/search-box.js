59e4e8045c78bc9cb298a6a1dc26e481684b3ce7
Romserg <serg.romankov@gmail.com>
2017-06-15 00:00:28 +0300

    ...

--- src/components/search-box/search-box.js
+++ src/components/search-box/search-box.js
@@ -3,20 +3,20 @@
 //     document.getElementsByClassName('search-box-input').style.display = 'block';
 //   },
 // };
-const show = new Vue({
-	el: '#show',
-	data: {
-	name: 'search-box.js',
-},
-  // define methods under the `methods` object
-	methods: {
-	show(event) {
-      // `this` inside methods points to the Vue instance
-	document.getElementsByClassName('search-box-input').style.display = 'block';
-      // `event` is the native DOM event
-	if (event) {
-	document.getElementsByClassName('search-box-input').style.display = 'block';
-}
-},
-},
-});
+// const show = new Vue({
+// 	el: '#show',
+// 	data: {
+// 	name: 'search-box.js',
+// },
+//   // define methods under the `methods` object
+// 	methods: {
+// 	show(event) {
+//       // `this` inside methods points to the Vue instance
+// 	document.getElementsByClassName('search-box-input').style.display = 'block';
+//       // `event` is the native DOM event
+// 	if (event) {
+// 	document.getElementsByClassName('search-box-input').style.display = 'block';
+// }
+// },
+// },
+// });
