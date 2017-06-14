ff19dbfefe176b3694f69a86a36a33da957426d6
Romserg <serg.romankov@gmail.com>
2017-06-15 01:55:23 +0300

    fixed 2.0

--- src/pages/ui-kit.vue
+++ src/pages/ui-kit.vue
@@ -1,40 +1,41 @@
 <script>
-    import PageHeader from '../components/page-header/page-header.vue';
-    import ImageContent from '../components/image-content/image-content.vue';
-    import TagItem from '../components/tag-item/tag-item.vue';
-    import SearchBox from '../components/search-box/search-box.vue';
-    import ButtonControl from '../components/button-control/button-control.vue';
-    import MainLogo from '../components/main-logo/main-logo.vue';
-    import ContentWrapper from '../components/content-wrapper/content-wrapper.vue';
-	
-    export default {
-	components: { PageHeader, ImageContent, TagItem, SearchBox, ButtonControl, MainLogo, ContentWrapper }
-    };
+	import PageHeader from '../components/page-header/page-header.vue';
+	import ImageContent from '../components/image-content/image-content.vue';
+	import TagItem from '../components/tag-item/tag-item.vue';
+	import SearchBox from '../components/search-box/search-box.vue';
+	import ButtonControl from '../components/button-control/button-control.vue';
+	import MainLogo from '../components/main-logo/main-logo.vue';
+	import ContentWrapper from '../components/content-wrapper/content-wrapper.vue';
+	import TagFooter from '../components/tag-footer/tag-footer.vue';
+
+	export default {
+		components: { PageHeader, ImageContent, TagItem, SearchBox, ButtonControl, MainLogo, ContentWrapper, TagFooter }
+	};
 </script>
 
 <template>
-    <body>
-    <content-wrapper> 
+	<body>
+	<content-wrapper> 
 
+		<page-header />
+		
+		UI Kit
 
-   <page-header />
-        
-        UI Kit
+	    <main-logo></main-logo>
 
-        <main-logo></main-logo>
+		<image-content></image-content>
 
-        <image-content></image-content>
+		<tag-item></tag-item>
 
-        <tag-item></tag-item>
+		<a href="">Open Sans</a>
 
-        <a href="">Open Sans</a>
+		<search-box></search-box>
+		
+		<button-control></button-control>
 
-        <search-box></search-box>
-        
-        <button-control></button-control>
+        <tag-footer></tag-footer>
 
-        </content-wrapper>
+	</content-wrapper>
 
-    </body>
+	</body>
 </template>
-
