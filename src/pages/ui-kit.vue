b20599fd4a639dc180c04d75f6977128dae6e520
Diana Ostrenko <ostrenkodiana1@gmail.com>
2017-06-14 13:14:51 +0300

    try smth new

--- src/pages/ui-kit.vue
+++ src/pages/ui-kit.vue
@@ -1,42 +1,41 @@
 <script>
-	import PageHeader from '../components/page-header/page-header.vue';
-	import ImageContent from '../components/image-content/image-content.vue';
-	import TagItem from '../components/tag-item/tag-item.vue';
-	import SearchBox from '../components/search-box/search-box.vue';
-	import ButtonControl from '../components/button-control/button-control.vue';
+    import PageHeader from '../components/page-header/page-header.vue';
+    import ImageContent from '../components/image-content/image-content.vue';
+    import TagItem from '../components/tag-item/tag-item.vue';
+    import SearchBox from '../components/search-box/search-box.vue';
+    import ButtonControl from '../components/button-control/button-control.vue';
     import MainLogo from '../components/main-logo/main-logo.vue';
-	import ContentWrapper from '../components/content-wrapper/content-wrapper.vue';
+    import ContentWrapper from '../components/content-wrapper/content-wrapper.vue';
 
-	export default {
-		components: { PageHeader, ImageContent, TagItem, SearchBox, ButtonControl, MainLogo, ContentWrapper }
-	};
+    export default {
+        components: { PageHeader, ImageContent, TagItem, SearchBox, ButtonControl, MainLogo, ContentWrapper }
+    };
 </script>
 
 <template>
-	<body>
-	<content-wrapper> 
+    <body>
+    <content-wrapper> 
 
-		<page-header />
-		
-		UI Kit
+   <page-header />
+        
+        UI Kit
 
-	    <main-logo></main-logo>
+        <main-logo></main-logo>
 
-		
+        <button>Кнопка с текстом</button>
 
-		<image-content></image-content>
+        <image-content></image-content>
 
-		<tag-item></tag-item>
+        <tag-item></tag-item>
 
-		
+        <a href="">Open Sans</a>
 
-		<a href="">Open Sans</a>
+        <search-box></search-box>
+        
+        <button-control></button-control>
 
-		<search-box></search-box>
-		
-		<button-control></button-control>
+        </content-wrapper>
 
-		</content-wrapper>
-		
-	</body>
+    </body>
 </template>
+
