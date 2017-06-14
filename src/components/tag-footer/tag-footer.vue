2a792b8f29eada62e1af63f17f147617b922a794
Romserg <serg.romankov@gmail.com>
2017-06-15 01:53:31 +0300

    fixed

---
+++ src/components/tag-footer/tag-footer.vue
@@ -0,0 +1,61 @@
+<style src="./tag-footer.css" />
+
+<template>
+  <div class="tag-footer">
+      <div class="tag-content"> 
+            <div class="tag-content-coll1 tag-coll">
+                <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec.</p>
+                <div> 
+                    <div class="phone">Phone: 182 2569 5896</div>
+                    <div class="mail">e-mail: info@modu.versus</div>
+                </div>
+            </div>
+
+            <div class="tag-content-coll2 tag-coll">
+                <div class="companymain">
+                    <div class="company">Company</div>
+                    <div class="company1">  
+                          <div class="companymenu">&gt; About</div>
+                          <div class="companymenu">&gt; FAQ</div>
+                          <div class="companymenu">&gt; Conatact</div>
+                          <div class="companymenu">&gt; Terms</div>
+                          <div class="companymenu">&gt; Privacy</div>
+                          <div class="companymenu">&gt; Testimonials</div>
+                    </div>
+                    <div class="community">Community</div>
+                    <div class="community1">
+                          <div class="communitymenu">&gt; Blog</div>
+                          <div class="communitymenu">&gt; Forum</div>
+                          <div class="communitymenu">&gt; Support</div>
+                          <div class="communitymenu">&gt; Newsleter</div>
+                    </div>
+                </div>       
+             </div>
+             <div class="tag-content-coll3 tag-coll">
+                    <div class="imageblog">
+                              <div class="member">
+                                        <div class="image1"><img src="./foto.png" alt="">
+                                        <div class="text1">Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                                        </div>
+                                        <div class="text1">26 May, 2013</div>    
+                                        </div> 
+                                        <div class="image2"><img src="./foto.png" alt="">
+                                        <div class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                                        </div>
+                                        <div class="text1">26 May, 2013</div>  
+                                        </div>     
+                              </div>  
+                    </div>
+              </div>
+      </div>
+      <div class="tag-bottom-line"> 
+          <div class="ModusVersus">2013  ModusVersus</div>
+              <div class="logopicture">
+                  <div class="facebook"><img src="./Facebook.png" alt=""></div>
+                  <div class="google"><img src="./google.png" alt=""></div>
+                  <div class="twetter"><img src="./twetter.png" alt=""></div>
+                  <div class="rss"><img src="./rss.png" alt=""></div>
+              </div>  
+      </div>
+   </div>
+</template>
