d9b69a6357d86ae48ac47067d9317293b66aee36
Romserg <serg.romankov@gmail.com>
2017-06-14 16:58:38 +0300

    complete search-box

--- src/components/search-box/search-box.vue
+++ src/components/search-box/search-box.vue
@@ -2,7 +2,9 @@
 
 <template>
   <div class="search-box">
-	 <div class="search-box-button"><slot></slot></div> 
-	 <div class="search-box-input"><slot><input type="text"></slot></div>
+	 <button class="search-box-button"></button>
+	 <div class="search-box-input">
+	 	<input type="text" placeholder="Search the site">
+	 </div>
   </div>  
 </template>
