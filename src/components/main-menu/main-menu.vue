5d53e2ddbd7c07aa5f5f9aefa770066111435495
Galina <tris151316@gmail.com>
2017-06-16 02:09:30 +0300

    Connections of the font, style and type of the main menu.

--- src/components/main-menu/main-menu.vue
+++ src/components/main-menu/main-menu.vue
@@ -1,29 +1,19 @@
 <style src="./main-menu.css" />
 
 <template>
-	<nav class="main-menu">
-		<router-link to="/index" active-class="current">Home</router-link>
-		<router-link to="/about" active-class="current">About</router-link>
-		<router-link to="/services" active-class="current">Services</router-link>
-		<router-link to="/portfolio" active-class="current">Portfolio</router-link>
-		<router-link to="/portfolio-2" active-class="current">Portfolio 2</router-link>
-		<router-link to="/portfolio-3" active-class="current">Portfolio 3</router-link> 
-		<router-link to="/portfolio-4" active-class="current">Portfolio 4</router-link>
-		<router-link to="/blog" active-class="current">Blog</router-link>
-		<router-link to="/ui-kit" active-class="current">Examples</router-link>
-	</nav>
+		<nav class="main-menu">
+			<ul class="menu-list">
+				<li class="menu-item"<router-link to="/index" active-class="current">Home</router-link></li>
+				<li class="menu-item"<router-link to="/about" active-class="current">About</router-link></li>
+				<li class="menu-item"<router-link to="/services" active-class="current">Services</router-link></li>
+				<li class="menu-item"<router-link to="/portfolio" active-class="current">Portfolio</router-link></li>
+				<li class="menu-item-dropdown"<router-link to="/portfolio-2" active-class="current">Portfolio 2</router-link></li>
+				<li class="menu-item-dropdown"<router-link to="/portfolio-3" active-class="current">Portfolio 3</router-link></li>
+				<li class="menu-item-dropdown"<router-link to="/portfolio-4" active-class="current">Portfolio 4</router-link></li>
+				<li class="menu-item"<router-link to="/blog" active-class="current">Blog</router-link></li>
+				<li class="menu-item"<router-link to="/features" active-class="current">Features</router-link></li>
+				<li class="menu-item"<router-link to="/contacts" active-class="current"Contacts</router-link></li>
+				<li class="menu-item"<router-link to="/ui-kit" active-class="current">Examples</router-link></li>
+			</ul>
+		</nav>
 </template>
-</template>
-/*<template>
-	<nav class="main-menu">
-		<router-link to="/index" active-class="current">Main</router-link>
-		<router-link to="/contacts" active-class="current">Contacts</router-link>
-		<router-link to="/blog" active-class="current">Blog</router-link>
-		<router-link to="/portfolio" active-class="current">Portfolio</router-link>
-		<router-link to="/portfolio-2" active-class="current">Portfolio 2</router-link>
-		<router-link to="/portfolio-3" active-class="current">Portfolio 3</router-link> 
-		<router-link to="/portfolio-4" active-class="current">Portfolio 4</router-link>
-		<router-link to="/ui-kit" active-class="current">Examples</router-link>
-	</nav>
-</template>
-</template>*/
