2e19941a3ec346952126468122cda8462137c61e
slobodyanik <slobodyanik.vyacheslav@gmail.com>
2017-06-15 15:57:18 +0300

    update pagination

--- src/components/pagination-menu/pagination-menu.vue
+++ src/components/pagination-menu/pagination-menu.vue
@@ -1,18 +1,19 @@
 
 <style src="./pagination-menu.css" />
 <template>
-<div <class="pagination">
-<a class="prev" href= "#">&laquo;</a>
+<div class="pagination"> 
+<a class="prev" href= "#">❮</a>  
 <a href= "#">1</a>
-<a href= "#">2</a>  
+<a class="active" href= "#">2</a>  
 <a href= "#">3</a>
 <a href= "#">4</a>
 <a href= "#">5</a>
-<span>&hellip;</span> 
+<span> &hellip;</span> 
 <a href= "#">10</a>
 <a href= "#">11</a>
 <a href= "#">12</a>
-<a class="next" href= "#">&raquo;</a>
+<a class="next" href= "#">❯</a>
 </div>
+
 </<template>
   
