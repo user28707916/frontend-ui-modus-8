ff688b6ebd1e61dae7d8051e7dae999c9ae0fd1a
Galina <tris151316@gmail.com>
2017-06-15 23:33:12 +0300

    main-menu v2

--- src/components/main-menu/main-menu.vue
+++ src/components/main-menu/main-menu.vue
@@ -2,6 +2,20 @@
 
 <template>
 	<nav class="main-menu">
+		<router-link to="/index" active-class="current">Home</router-link>
+		<router-link to="/About" active-class="current">About</router-link>
+		<router-link to="/blog" active-class="current">Services</router-link>
+		<router-link to="/portfolio" active-class="current">Portfolio</router-link>
+		<router-link to="/portfolio-2" active-class="current">Portfolio 2</router-link>
+		<router-link to="/portfolio-3" active-class="current">Portfolio 3</router-link> 
+		<router-link to="/portfolio-4" active-class="current">Portfolio 4</router-link>
+		<router-link to="/blog" active-class="current">BLOG</router-link
+		<router-link to="/ui-kit" active-class="current">Examples</router-link>
+	</nav>
+</template>
+</template>
+/*<template>
+	<nav class="main-menu">
 		<router-link to="/index" active-class="current">Main</router-link>
 		<router-link to="/contacts" active-class="current">Contacts</router-link>
 		<router-link to="/blog" active-class="current">Blog</router-link>
@@ -12,4 +26,4 @@
 		<router-link to="/ui-kit" active-class="current">Examples</router-link>
 	</nav>
 </template>
-</template>
+</template>*/
