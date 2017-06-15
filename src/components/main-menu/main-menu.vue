2caa0bed40e1b11585b4fa02eb4802103b0f358a
Galina <tris151316@gmail.com>
2017-06-15 23:45:09 +0300

    main-menu v2.3

--- src/components/main-menu/main-menu.vue
+++ src/components/main-menu/main-menu.vue
@@ -3,13 +3,13 @@
 <template>
 	<nav class="main-menu">
 		<router-link to="/index" active-class="current">Home</router-link>
-		<router-link to="/About" active-class="current">About</router-link>
-		<router-link to="/blog" active-class="current">Services</router-link>
+		<router-link to="/about" active-class="current">About</router-link>
+		<router-link to="/services" active-class="current">Services</router-link>
 		<router-link to="/portfolio" active-class="current">Portfolio</router-link>
 		<router-link to="/portfolio-2" active-class="current">Portfolio 2</router-link>
 		<router-link to="/portfolio-3" active-class="current">Portfolio 3</router-link> 
 		<router-link to="/portfolio-4" active-class="current">Portfolio 4</router-link>
-		<router-link to="/blog" active-class="current">BLOG</router-link
+		<router-link to="/blog" active-class="current">Blog</router-link>
 		<router-link to="/ui-kit" active-class="current">Examples</router-link>
 	</nav>
 </template>
