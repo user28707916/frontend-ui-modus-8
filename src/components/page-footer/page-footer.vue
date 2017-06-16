f3667e7d600ffce2d31ad092cd583e5b88ea03a2
stas <kelen.omnel1@gmail.com>
2017-06-16 15:30:35 +0300

    comit

---
+++ src/components/page-footer/page-footer.vue
@@ -0,0 +1,66 @@
+<style src="./page-footer.css" />
+
+<template>
+  <div class="page-footer">
+      <div class="page-content"> 
+            <div class="page-content-coll1 tag-coll">
+                <div class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec.</div>
+                <div> 
+                    <div class="phone">Phone: 182 2569 5896</div>
+                    <div class="mail">e-mail: info@modu.versus</div>
+                </div>
+            </div>
+
+            <div class="page-content-coll2 tag-coll">
+                <div class="companymain">
+                    <h3 class="company">Company</h3>
+                    <ul class="company1">  
+                          <li class="companymenu">&gt; About</li>
+                          <li class="companymenu">&gt; FAQ</li>
+                          <li class="companymenu">&gt; Contact</li>
+                          <li class="companymenu">&gt; Terms</li>
+                          <li class="companymenu">&gt; Privacy</li>
+                          <li class="companymenu">&gt; Testimonials</li>
+                    </ul>
+                    <h3 class="community">Community</h3>
+                    <ul class="community1">
+                          <li class="communitymenu">&gt; Blog</li>
+                          <li class="communitymenu">&gt; Forum</li>
+                          <li class="communitymenu">&gt; Support</li>
+                          <li class="communitymenu">&gt; Newsleter</li>
+                    </ul>
+                </div>       
+             </div>
+             <div class="page-content-coll3 tag-coll">
+                    <div class="imageblog">
+					          <div class="member">
+							            <div class="blog">from the <strong>BLOG</strong></div>					        
+										<div class="image1"><img src="./page-png/foto.png" alt="">
+                                        <div class="text1">Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                                        </div>
+                                        <div class="text2">26 May, 2013</div>    
+                                        </div> 
+                                        <div class="image2"><img src="./page-png/foto.png" alt="">
+                                        <div class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                                        </div>
+                                        <div class="text2">26 May, 2013</div>  
+                                        </div>  
+									       
+                              </div>  
+                    </div>
+              </div>
+      </div>
+      <div class="page-bottom-line"> 
+           <div class="tagpicture">
+              <div class="ModusVersus">2013  ModusVersus</div>
+              <nav class="logopicture">
+                  <div class="facebook"><img src="./page-png/Facebook.png" alt=""></div>
+                  <div class="google"><img src="./page-png/google.png" alt=""></div>
+                  <div class="twetter"><img src="./page-png/twetter.png" alt=""></div>
+                  <div class="rss"><img src="./page-png/rss.png" alt=""></div>
+              </nav> 
+		   </div>   
+      </div>
+   </div>
+   
+</template>
