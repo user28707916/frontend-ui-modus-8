83fe13e0be4162b3b1f1db6ae666c8045f8a2f66
Diana Ostrenko <ostrenkodiana1@gmail.com>
2017-06-16 23:32:52 +0300

    header without menu

--- src/components/page-header/page-header.vue
+++ src/components/page-header/page-header.vue
@@ -2,12 +2,10 @@
 
 <script>
 	import MainLogo from '../main-logo/main-logo.vue';
-	import MainMenu from '../main-menu/main-menu.vue';
 	
 	export default {
 		components: {
-		MainLogo,
-		MainMenu
+		MainLogo
 		}
 	};
 </script>
@@ -19,7 +17,6 @@
 <header class="page-header">
 	
 	<main-logo class="header-logo"> </main-logo>
-	<main-menu class="header-menu">	</main-menu>
 	
 </header>
 </template>
