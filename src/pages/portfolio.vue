c335f62bb765eb83df79dc9d59e3ba702cdc373b
VovaNguyen11 <vovanguyen11@gmail.com>
2017-06-17 19:52:06 +0300

    Fix

--- src/pages/portfolio.vue
+++ src/pages/portfolio.vue
@@ -2,49 +2,31 @@
 
 <script>
 	import PageHeader from '../components/page-header/page-header.vue';
-<<<<<<< HEAD
-<<<<<<< HEAD
 	import TilesGrid from '../components/tiles-grid/tiles-grid.vue';
 	import TileSmall from '../components/tile-small/tile-small.vue';
-
-	export default {
-		components: { PageHeader, TilesGrid, TileSmall }
-=======
-=======
->>>>>>> 8c93273b4e07ddf248a0efb35ff89c21525b0fa4
 	import PageFooter from '../components/page-footer/page-footer.vue';
 	import ContentWrapper from '../components/content-wrapper/content-wrapper.vue';
 	import TagItem from '../components/tag-item/tag-item.vue';
 
 	export default {
-		components: {
-			PageHeader,
-			PageFooter,
-			ContentWrapper,
-			TagItem
-		}
-<<<<<<< HEAD
->>>>>>> 8c93273b4e07ddf248a0efb35ff89c21525b0fa4
-=======
->>>>>>> 8c93273b4e07ddf248a0efb35ff89c21525b0fa4
-	};
+		components: { 
+			PageHeader, TilesGrid, TileSmall, PageFooter, ContentWrapper, TagItem 
+		}	
+	}
+
 </script>
 
 <template>
 	<body>
-		
+
 		<page-header />
 
-		
 		<content-wrapper>
-		
 		
 		<h1>Portfolio</h1>
 
 		<tag-item></tag-item>
-<<<<<<< HEAD
 
-<<<<<<< HEAD
 		<tiles-grid>
 			<tile-small></tile-small>
 			<tile-small></tile-small>
@@ -56,14 +38,11 @@
 			<tile-small></tile-small>
 		</tiles-grid>
 
-=======
 		</content-wrapper>
 		<page-footer></page-footer>
->>>>>>> 8c93273b4e07ddf248a0efb35ff89c21525b0fa4
-=======
 
 		</content-wrapper>
 		<page-footer></page-footer>
->>>>>>> 8c93273b4e07ddf248a0efb35ff89c21525b0fa4
+
 	</body>
 </template>
