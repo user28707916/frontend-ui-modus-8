813ee33fa5478b9b40f87c18bf51f3f5e51c2fd6
Galina <tris151316@gmail.com>
2017-06-18 01:10:11 +0300

    Ready-made styles to breadcrumb-navigation , the layout on the screen to finish!

--- src/components/breadcrumbs-navigation/breadcrumbs-navigation.vue
+++ src/components/breadcrumbs-navigation/breadcrumbs-navigation.vue
@@ -1,7 +1,14 @@
 <style src="./breadcrumbs-navigation.css" />
 
 <template>
-	<div class="breadcrumbs-navigation.css">
-		<slot> </slot>
+	<div class="breadcrumbs-navigation">
+		<ul class="breadcrumb-navigation">
+			<li>
+				<router-link to="/index">Home</router-link>
+			</li>
+			<li>
+				<router-link to="/portfolio">Pordtfolio</router-link>
+			</li>
+		</ul>
 	</div>
 </template>
