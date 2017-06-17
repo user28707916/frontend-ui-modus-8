e6af07a218f5a009d1e120c99c4c6ec6be09013b
VovaNguyen11 <vovanguyen11@gmail.com>
2017-06-17 22:08:37 +0300

    Add large tile

--- src/pages/portfolio.vue
+++ src/pages/portfolio.vue
@@ -5,6 +5,7 @@
 	import TilesGrid from '../components/tiles-grid/tiles-grid.vue';
 	import TileSmall from '../components/tile-small/tile-small.vue';
 	import TileMedium from '../components/tile-medium/tile-medium.vue';
+	import TileLarge from '../components/tile-large/tile-large.vue';
 	import PageFooter from '../components/page-footer/page-footer.vue';
 	import ContentWrapper from '../components/content-wrapper/content-wrapper.vue';
 	import TagItem from '../components/tag-item/tag-item.vue';
@@ -12,7 +13,7 @@
 
 	export default {
 		components: { 
-			PageHeader, TilesGrid, TileSmall, TileMedium, PageFooter, ContentWrapper, TagItem, ImageContent 
+			PageHeader, TilesGrid, TileSmall, TileMedium, TileLarge, PageFooter, ContentWrapper, TagItem, ImageContent 
 		}	
 	}
 
@@ -37,6 +38,8 @@
 			<tile-medium></tile-medium>
 			<tile-medium></tile-medium>
 			<tile-medium></tile-medium>
+			<tile-large></tile-large>
+		
 		</tiles-grid>
 
 		</content-wrapper>
