f30e0f715ba96d5990a001707e26b451e0c8734f
Galina <tris151316@gmail.com>
2017-06-17 20:22:47 +0300

    Examples to ui-kit from index

--- src/components/main-menu/main-menu.vue
+++ src/components/main-menu/main-menu.vue
@@ -1,17 +1,24 @@
 <style src="./main-menu.css" />
 
 <template>
-		<nav class="main-menu">
-			<ul class="menu-list">
-				<li class="menu-item "<router-link to="/index" active-class="current">Home</router-link></li>
-				<li class="menu-item"<router-link to="/about" active-class="current">About</router-link></li>
-				<li class="menu-item"<router-link to="/services"active-class="current">Services</router-link></li>
-				<li class="menu-item"<router-link to="/portfolio"active-class="current">Portfolio</router-link></li>
-				<li class="menu-item"<router-link to="/blog" active-class="current">Blog</router-link></li>
-				<li class="menu-item"<router-link to="/features"active-class="current">Features</router-link></li>
-				<li class="menu-item"<router-link to="/contacts" active-class="current"Contacts</router-link></li>
-			
-			</ul>
-			</nav>
-			
+	<nav class="main-menu">
+		<ul class="menu-list">
+			<li class="menu-item " <router-link to="/index" active-class="current">Home</router-link>
+			</li>
+			<li class="menu-item" <router-link to="/about" active-class="current">About</router-link>
+			</li>
+			<li class="menu-item" <router-link to="/services" active-class="current">Services</router-link>
+			</li>
+			<li class="menu-item" <router-link to="/portfolio" active-class="current">Portfolio</router-link>
+			</li>
+			<li class="menu-item" <router-link to="/blog" active-class="current">Blog</router-link>
+			</li>
+			<li class="menu-item" <router-link to="/features" active-class="current">Features</router-link>
+			</li>
+			<li class="menu-item" <router-link to="/contacts" active-class="current" Contacts</router-link></li>
+			<router-link to="/ui-kit" <li class="menu-item" <router-link to="/ui-kit" active-class="current">Examples</router-link>
+			</li> в main-menu.vue
+	
+		</ul>
+	</nav>
 </template>
