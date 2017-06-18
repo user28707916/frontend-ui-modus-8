86cf18e4dffd7b011306670131b93f42bfcf6817
Galina <tris151316@gmail.com>
2017-06-18 10:39:59 +0300

    Top-bar portfolio connect

--- src/pages/portfolio.vue
+++ src/pages/portfolio.vue
@@ -1,53 +1,53 @@
 <style src="../styles/common.css"/>
 
 <script>
-	import PageHeader from '../components/page-header/page-header.vue';
-	import TilesGrid from '../components/tiles-grid/tiles-grid.vue';
-	import TileSmall from '../components/tile-small/tile-small.vue';
-	import TileMedium from '../components/tile-medium/tile-medium.vue';
-	import TileLarge from '../components/tile-large/tile-large.vue';
-	import PageFooter from '../components/page-footer/page-footer.vue';
-	import ContentWrapper from '../components/content-wrapper/content-wrapper.vue';
-	import TagItem from '../components/tag-item/tag-item.vue';
-	import ImageContent from '../components/image-content/image-content.vue';
+import PageHeader from '../components/page-header/page-header.vue';
+import TilesGrid from '../components/tiles-grid/tiles-grid.vue';
+import TileSmall from '../components/tile-small/tile-small.vue';
+import TileMedium from '../components/tile-medium/tile-medium.vue';
+import TileLarge from '../components/tile-large/tile-large.vue';
+import PageFooter from '../components/page-footer/page-footer.vue';
+import ContentWrapper from '../components/content-wrapper/content-wrapper.vue';
+import TagItem from '../components/tag-item/tag-item.vue';
+import ImageContent from '../components/image-content/image-content.vue';
+import BreadcrumbsNavigation from '../components/breadcrumbs-navigation/breadcrumbs-navigation.vue';
+import TopBar from '../components/top-bar/top-bar.vue';
 
-	export default {
-		components: { 
-			PageHeader, TilesGrid, TileSmall, TileMedium, TileLarge, PageFooter, ContentWrapper, TagItem, ImageContent 
-		}	
+export default {
+	components: {
+		PageHeader, TilesGrid, TileSmall, TileMedium, TileLarge, PageFooter, ContentWrapper, TagItem, ImageContent, BreadcrumbsNavigation, TopBar
 	}
+}
 
 </script>
 
 <template>
 	<body>
-
+	
 		<page-header />
-
-		<content-wrapper>
-		
+		<top-bar/>
+		<breadcrumbs-navigation/>
 		<h1>Portfolio</h1>
-
-		<tag-item></tag-item>
-
-		<tiles-grid>
-			<tile-small></tile-small>
-			<tile-small></tile-small>
-			<tile-small></tile-small>
-			<tile-small></tile-small>
-			<tile-medium></tile-medium>
-			<tile-medium></tile-medium>
-			<tile-medium></tile-medium>
-			<tile-large></tile-large>
-			<tile-large></tile-large>
-		</tiles-grid>
-		
+	
+		<content-wrapper>
+	
+			<tag-item></tag-item>
+	
+			<tiles-grid>
+				<tile-small></tile-small>
+				<tile-small></tile-small>
+				<tile-small></tile-small>
+				<tile-small></tile-small>
+				<tile-medium></tile-medium>
+				<tile-medium></tile-medium>
+				<tile-medium></tile-medium>
+				<tile-large></tile-large>
+				<tile-large></tile-large>
+			</tiles-grid>
+	
 		</content-wrapper>
 		<page-footer></page-footer>
-
-<<<<<<< HEAD
-
-=======
->>>>>>> 4cceda26bb5ffda006595382f93e79d38128e854
+	
+		<<<<<<< HEAD=======>>>>>>> 4cceda26bb5ffda006595382f93e79d38128e854
 	</body>
 </template>
