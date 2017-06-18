ae927f1f732143e733f37f3ff093b60a3a425e96
Galina <tris151316@gmail.com>
2017-06-18 13:04:30 +0300

    try

--- src/components/top-bar/top-bar.vue
+++ src/components/top-bar/top-bar.vue
@@ -1,7 +1,21 @@
+
+
+<script>
+import BreadcrumbsNavigation from '../breadcrumbs-navigation/breadcrumbs-navigation.vue';
+
+export default {
+	components: {
+		BreadcrumbsNavigation
+	}
+};
+
+</script>
 <style src="./top-bar.css" />
 
 <template>
-	<div class="top-bar ">
-		
+	<div class="top-bar">
+		<breadcrumbs-navigation/>
+		<h1> active-class="current"</h1>
 	</div>
-</templat
+</template>
+  
