3ce4ef75b0db179efc32b776cf08e68f1670b927
slobodyanik <slobodyanik.vyacheslav@gmail.com>
2017-06-20 16:36:14 +0300

    fix bugs with pagination

--- src/components/pagination-menu/pagination-menu.vue
+++ src/components/pagination-menu/pagination-menu.vue
@@ -2,17 +2,17 @@
 <style src="./pagination-menu.css" />
 <template>
 	<div class="pagination">
-		<a class="prev" href="#"></a>
-		<a href="#">1</a>
-		<a class="active" href="#">2</a>
-		<a href="#">3</a>
-		<a href="#">4</a>
-		<a href="#">5</a>
-		<span> &hellip;</span>
-		<a href="#">10</a>
-		<a href="#">11</a>
-		<a href="#">12</a>
-		<a class="next" href="#"></a>
+		<a class="navlink prev" href="#"></a>
+		<a class="navlink" href="#">1</a>
+		<a class="navlink active" href="#">2</a>
+		<a class="navlink" href="#">3</a>
+		<a class="navlink" href="#">4</a>
+		<a class="navlink" href="#">5</a>
+		<span class="navlink"> &hellip;</span>
+		<a class="navlink" href="#">10</a>
+		<a class="navlink" href="#">11</a>
+		<a class="navlink" href="#">12</a>
+		<a class="next navlink" href="#"></a>
 	</div>
 </template>
   
