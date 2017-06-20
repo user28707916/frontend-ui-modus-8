d40115601d1220f94fc6f56d00fccfb38e04c4af
Gusakovskyi <gusakovskyi@gmail.com>
2017-06-20 20:48:55 +0300

    Teaser-item done. Update image-content (new styles for tiles and teaser)

---
+++ src/components/teaser-item/teaser-item.vue
@@ -0,0 +1,27 @@
+<style src="./teaser-item.css" />
+
+<script>
+import ImageContent from '../image-content/image-content.vue';
+import LinkExternal from '../link-external/link-external.vue';
+import ButtonControl from '../button-control/button-control.vue';
+
+export default {
+	components: {
+		ImageContent,
+		LinkExternal,
+		ButtonControl
+	}
+};
+</script>
+
+<template>
+	<div class="teaser-item" >
+		<image-content src="./components/image-content/image-placeholder.png"></image-content>
+		<h4>Donec faucibus ultricies congue</h4>
+		<p>Maecenas eget turpis turpis. Nunc vel metus augue. Aenean euismod cursus ligula eget dapibus. Praesent vel erat in tortor placerat dignissim. Duis dapibus aliquam mi, eget euismod sem scelerisque ut. Vivamus at elit quis urna adipiscing iaculis. Curabitur vitae velit in neque dictum blandit. Proin in iaculis neque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+		<LinkExternal>www.project.dom</LinkExternal>
+
+		<button-control>View Details</button-control>
+
+	</div>
+</template>
