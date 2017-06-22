21d6309445822cd9651e43edac38e1599df95746
Romserg <serg.romankov@gmail.com>
2017-06-22 18:27:47 +0300

    complete motivation-info

--- src/components/motivation-info/motivation-info.vue
+++ src/components/motivation-info/motivation-info.vue
@@ -7,10 +7,22 @@
 	  	<h2>Why Modus Versus?</h2>
 		<span>Capacitance cascading integer reflective interface data development high bus cache dithering transponder.</span>
 	  </div>
-	  <div class="allwindows">
-	  	<div class="front-window transformed-left"><img src="" alt="Your Image"></div>
-	  	<div class="front-window transformed-right"><img src="" alt="Your Image"></div> 
-		<div class="front-window"><img src="" alt="Your Image"></div> 
-	  </div>
+	  	<div class="allwindows">
+	  		<div class="front-window transformed-left">
+			  	<div class="image-wrapper"><span class="text-field">your image</span>
+			  		<img src="" alt="">
+				</div>
+			</div>
+	  		<div class="front-window transformed-right">
+		  		<div class="image-wrapper"><span class="text-field">your image</span>
+		  			<img src="" alt="">
+				</div> 
+			</div>
+			<div class="front-window">
+				<div class="image-wrapper"><span class="text-field">your image</span>
+					<img src="" alt="">
+				</div> 
+			</div>
+	  	</div>
     </div>
 </template>
