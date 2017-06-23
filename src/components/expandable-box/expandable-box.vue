9436839cfb6a885f3e728b9b914a57b3f3858af2
Romserg <serg.romankov@gmail.com>
2017-06-24 00:31:44 +0300

    prepearing workspace for expandable box

--- src/components/expandable-box/expandable-box.vue
+++ src/components/expandable-box/expandable-box.vue
@@ -2,5 +2,9 @@
 
 <template>
 <div class="expandable-box">
+	<span class="accordion-title" v-on:click="toggle">Accordion Title</span>
+	<span class="text-message">Vivamus eget ante bibendum arcu vehicula ultricies. Integer venenatis mattis nisl, vitae pulvinar dui tempor.Ut eleifend libero sed neque rhoncus consequat. Maecenas tincidunt, augue et rutrum condimentum, libero lectus mattis orci, ut commodo.</span>
 </div>
 </template>
+
+<script src="./expandable-box.js"></script>
