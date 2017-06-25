157c92260250ed889ac43c1b97460baaea04f988
Gusakovskyi <gusakovskyi@gmail.com>
2017-06-26 00:06:31 +0300

    Update <address-info> and page "contacts"

--- src/components/address-info/address-info.vue
+++ src/components/address-info/address-info.vue
@@ -2,13 +2,9 @@
 
 
 <template>
-	<div class="address-info" >
-		<span class="contacts-term">Address</span>
-		<div class="contact_list">
-			<div class="contact_item_loc">Ut interdum tristique est com modo pharetra.</div>
-			<div class="contact_item_tel"><a href="tel:11156985698">111 5698 5698</a></div>
-			<div class="contact_item_mail"><a href="mailto:modus@versus.com">modus@versus.com</a></div>
-		</div>
-
-	</div>
+	<address class="address-info" >
+		<div class="contact_item_loc">Ut interdum tristique est com modo pharetra.</div>
+		<div class="contact_item_tel"><a href="tel:11156985698">111 5698 5698</a></div>
+		<div class="contact_item_mail"><a href="mailto:modus@versus.com">modus@versus.com</a></div>
+	</address>
 </template>
