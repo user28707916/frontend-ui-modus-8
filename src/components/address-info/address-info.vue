496b7b09ff2d39e4e843d450a4414bf75a327d23
Gusakovskyi <gusakovskyi@gmail.com>
2017-06-25 16:52:12 +0300

    Fix address-info.

--- src/components/address-info/address-info.vue
+++ src/components/address-info/address-info.vue
@@ -3,12 +3,12 @@
 
 <template>
 	<div class="address-info" >
-		<h4>Address</h4>
-		<ul class="contact_list">
-			<li class="contact_item_loc">Ut interdum tristique est com modo pharetra.</li>
-			<li class="contact_item_tel"><a href="tel:11156985698">111 5698 5698</a></li>
-			<li class="contact_item_mail"><a href="mailto:modus@versus.com">modus@versus.com</a></li>
-		</ul>
+		<p class="contacts-term">Address</p>
+		<div class="contact_list">
+			<div class="contact_item_loc">Ut interdum tristique est com modo pharetra.</div>
+			<div class="contact_item_tel"><a href="tel:11156985698">111 5698 5698</a></div>
+			<div class="contact_item_mail"><a href="mailto:modus@versus.com">modus@versus.com</a></div>
+		</div>
 
 	</div>
 </template>
