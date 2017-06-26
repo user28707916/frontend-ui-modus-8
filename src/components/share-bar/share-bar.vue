46107da5087fa523942ce494d2e795d659906c00
ressiver <diospyros.digyna@gmail.com>
2017-06-26 17:07:50 +0300

    change style and structure share-bar

--- src/components/share-bar/share-bar.vue
+++ src/components/share-bar/share-bar.vue
@@ -4,19 +4,24 @@
 
 	<div class="share-bar">
 
-		<ul>
-			<li>share</li>
-			<li><a href=""><img src="./image/icon_facebook.png" alt="facebook"></a></li>
-			<li><a href=""><img src="./image/icon_twitter.png" alt="twitter"></a></li>
-			<li><a href=""><img src="./image/icon_google.png" alt="google+"></a></li>
-			<li><a href=""><img src="./image/icon_linkedin.png" alt="linkedin"></a></li>
-		</ul>
+		<div class="text-share">share</div>
 
-		<ul class="button-bar">
-			<li><button type="button" class="button-left"></button></li>
-			<li><button type="button" class="button-right"></button></li>
-		</ul>
-		
+		<div class="link-container">
+			<ul>
+				<li><a href=""><img src="./image/icon_facebook.png" alt="facebook"></a></li>
+				<li><a href=""><img src="./image/icon_twitter.png" alt="twitter"></a></li>
+				<li><a href=""><img src="./image/icon_google.png" alt="google+"></a></li>
+				<li><a href=""><img src="./image/icon_linkedin.png" alt="linkedin"></a></li>
+			</ul>
+		</div>
+
+		<div class="button-container">
+
+			<button type="button" class="button-left"></button>
+			<button type="button" class="button-right"></button>
+			
+		</div>
+
 	</div>
 
 </template>
