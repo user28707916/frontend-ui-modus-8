c9929b9017a77780ec9e45ff3ed7552d2f8e14ad
constgen <const.gen@gmail.com>
2017-07-23 17:14:22 +0300

    Remove font owesome

--- src/body.vue
+++ src/body.vue
@@ -5,7 +5,6 @@
 </script>
 <style src="./styles/typography.css" />
 <style src="./styles/common.css" />
-<style src="./components/font-awesome/css/font-awesome.min.css" />
 
 <template>
 	<router-view></router-view>
