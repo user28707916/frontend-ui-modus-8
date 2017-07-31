e04b9fd1acd5305fcd9e39df6146708cf042a8e2
Romserg <serg.romankov@gmail.com>
2017-07-31 21:35:25 +0300

    create new component accordion-list

---
+++ src/components/accordion-list/accordion-list.vue
@@ -0,0 +1,18 @@
+<script>
+import PageHeader from '../components/page-header/page-header.vue';
+
+export default {
+	components: { ExpandableBox }
+};
+</script>
+<style src="./accordion-list.css"/>
+
+<template>
+<div class="accordion-list" :class="{expanded: expanded}">
+	<expandable-box></expandable-box>
+	<expandable-box></expandable-box>
+	<expandable-box></expandable-box>
+</div>
+</template>
+
+<script src="./accordion-list.js"></script>
