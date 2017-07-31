43a651a9cf4908b5fbaea1cd7b36a535f6dfb24a
ostenkodiana <ostrenkodiana1@gmail.com>
2017-07-31 12:48:32 +0300

    responsive blog-page, but without blog-side-bar

--- src/components/blog-post/blog-post.vue
+++ src/components/blog-post/blog-post.vue
@@ -16,28 +16,25 @@ export default {
 </script>
 
 <template>
-<content-wrapper>
-
 	<div class="blog-post">
 		<div class="datum">
 			<span class="datum-month"> May</span>
-		<span class="datum-date">26</span>
+			<span class="datum-date">26</span>
 		</div>
-
+	
 		<image-placeholder class="blog-image-placeholder"></image-placeholder>
-
+	
 		<article class="blog-article">
-
-		<blog-post-header/>
-
-		<p class="blog-post-paragraph">
-			<slot> Maecenas eget turpis turpis. Nunc vel metus augue. Aenean euismod cursus ligula eget dapibus. Praesent vel erat in tortor placerat dignissim. Duis dapibus aliquam mi, eget euismod sem scelerisque ut. Vivamus at elit quis urna adipiscing iaculis. Curabitur vitae velit in neque dictum blandit. Proin in iaculis neque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-			</slot>
-		</p>
-		<div class="blog-post-link">
-			<a href="#">Continue reading</a>
-		 </div>
+	
+			<blog-post-header/>
+	
+			<p class="blog-post-paragraph">
+				<slot> Maecenas eget turpis turpis. Nunc vel metus augue. Aenean euismod cursus ligula eget dapibus. Praesent vel erat in tortor placerat dignissim. Duis dapibus aliquam mi, eget euismod sem scelerisque ut. Vivamus at elit quis urna adipiscing iaculis. Curabitur vitae velit in neque dictum blandit. Proin in iaculis neque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+				</slot>
+			</p>
+			<div class="blog-post-link">
+				<a href="#">Continue reading</a>
+			</div>
 		</article>
 	</div>
-</content-wrapper>
 </template>
--- src/pages/blog.vue
+++ src/pages/blog.vue
@@ -1,48 +1,50 @@
 <script>
-	import PageHeader from '../components/page-header/page-header.vue';
-	import PageFooter from '../components/page-footer/page-footer.vue';
-	import ContentWrapper from '../components/content-wrapper/content-wrapper.vue';
-	import TopBar from '../components/top-bar/top-bar.vue';
-	import BlogPost from '../components/blog-post/blog-post.vue';
-	import ImagePlaceholder from '../components/image-placeholder/image-placeholder.vue';
-	import BlogSideBar from '../components/blog-side-bar/blog-side-bar.vue';
-	import PaginationMenu from '../components/pagination-menu/pagination-menu.vue';
-	
+import PageHeader from '../components/page-header/page-header.vue';
+import PageFooter from '../components/page-footer/page-footer.vue';
+import ContentWrapper from '../components/content-wrapper/content-wrapper.vue';
+import TopBar from '../components/top-bar/top-bar.vue';
+import BlogPost from '../components/blog-post/blog-post.vue';
+import ImagePlaceholder from '../components/image-placeholder/image-placeholder.vue';
+import BlogSideBar from '../components/blog-side-bar/blog-side-bar.vue';
+import PaginationMenu from '../components/pagination-menu/pagination-menu.vue';
 
-	export default {
-		components: {
-			PageHeader,
-			TopBar,
-			ContentWrapper,
-			ImagePlaceholder,
-			BlogSideBar,
-			BlogPost,
-			PaginationMenu,
-			PageFooter
-		}
-	};
+
+export default {
+	components: {
+		PageHeader,
+		TopBar,
+		ContentWrapper,
+		ImagePlaceholder,
+		BlogSideBar,
+		BlogPost,
+		PaginationMenu,
+		PageFooter
+	}
+};
 </script>
 
 <template>
 	<body>
 		<page-header />
-		<top-bar><h1 class="naiming-top-bar">Blog</h1></top-bar>
-			<content-wrapper>
-				<blog-post></blog-post>
-				<blog-post>
-					<slide-show></slide-show>
-				</blog-post>
-				<blog-post>
-					<video-content></video-content>
-				</blog-post>
-				<blog-post></blog-post>
-				<blog-post>
-					<audio-content></audio-content>
-				</blog-post>
-				<blog-side-bar class="blog-side-bar-right"></blog-side-bar>
-				<pagination-menu></pagination-menu>
-			</content-wrapper>
-		
+		<top-bar>
+			<h1 class="naiming-top-bar">Blog</h1>
+		</top-bar>
+		<content-wrapper>
+			<blog-post></blog-post>
+			<blog-post>
+				<slide-show></slide-show>
+			</blog-post>
+			<blog-post>
+				<video-content></video-content>
+			</blog-post>
+			<blog-post></blog-post>
+			<blog-post>
+				<audio-content></audio-content>
+			</blog-post>
+			<blog-side-bar class="blog-side-bar-right"></blog-side-bar>
+			<pagination-menu></pagination-menu>
+		</content-wrapper>
+	
 		<page-footer />
 	</body>
 </template>
