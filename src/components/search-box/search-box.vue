47c5ea57dda69c8568de1dc0c6dc0ba6df7a7237
Romserg <serg.romankov@gmail.com>
2017-07-31 15:35:08 +0300

    again

--- src/components/search-box/search-box.vue
+++ src/components/search-box/search-box.vue
@@ -4,7 +4,7 @@
 	<div class="search-box">
 		<button class="search-box-button" v-on:click="toggle"></button>
 		<div class="search-box-input" :class="{shown: shown}">
-		<input type="text" placeholder="Search the site">
+			<input type="text" placeholder="Search the site">
 		</div>
 	</div>
 </template>
