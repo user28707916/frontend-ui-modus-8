d13c5e19f1a39fb1bf0c14ac796b3ad61ce8f298
Gusakovskyi <gusakovskyi@gmail.com>
2017-08-01 20:18:27 +0300

    Done component video-placeholder

---
+++ src/components/video-placeholder/video-placeholder.vue
@@ -0,0 +1,13 @@
+<script>
+import VideoContent from '../video-content/video-content.vue';
+
+export default {
+	components: {
+		VideoContent,
+	}
+};
+</script>
+
+<template>
+	<VideoContent src="./components/video-placeholder/video-placeholder.mp4"></VideoContent>
+</template>
