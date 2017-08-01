a8e5e418b4891a16eed0e4237620345d2ef25982
Gusakovskyi <gusakovskyi@gmail.com>
2017-08-01 20:22:17 +0300

    add component to ui-kit page

--- src/pages/ui-kit.vue
+++ src/pages/ui-kit.vue
@@ -1,6 +1,7 @@
 <script>
 import PageHeader from '../components/page-header/page-header.vue';
 import ImageContent from '../components/image-content/image-content.vue';
+import VideoPlaceholder from '../components/video-placeholder/video-placeholder.vue';
 import TagItem from '../components/tag-item/tag-item.vue';
 import SearchBox from '../components/search-box/search-box.vue';
 import ButtonControl from '../components/button-control/button-control.vue';
@@ -34,6 +35,7 @@ export default {
 	components: {
 		PageHeader,
 		ImageContent,
+		VideoPlaceholder,
 		TagItem,
 		SearchBox,
 		ButtonControl,
@@ -90,7 +92,8 @@ export default {
 			<LinkExternal></LinkExternal>
 			</br>
 			</content-wrapper>
-				<motivation-info></motivation-info>
+			<motivation-info></motivation-info>
+			<VideoPlaceholder></VideoPlaceholder>
 			<content-wrapper>
 			<expandable-box></expandable-box>
 			</br>
