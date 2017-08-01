43e858094a12a92fb0cf7b372b8de73fe9d78be6
Gusakovskyi <gusakovskyi@gmail.com>
2017-08-01 19:56:14 +0300

    done video-content component

---
+++ src/components/video-content/video-content.vue
@@ -0,0 +1,13 @@
+<style src="./video-content.css" />
+<script>
+	export default {
+		props: ['src']
+	}
+
+</script>
+
+<template>
+	<video width="420" height="315" controls>
+		<source :src="src">
+	</video>
+</template>
